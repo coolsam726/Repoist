9e5d02419b1eeb8abb24241e6a6f1a3096b4c6c2
Sam Maosa <coolsam726@users.noreply.github.com>
2017-07-20 14:10:37 +0300

    Update RepositoryMakeCommand.php
    Solution to Issue #1 on Laravel 5.4

--- src/Commands/RepositoryMakeCommand.php
+++ src/Commands/RepositoryMakeCommand.php
@@ -4,14 +4,15 @@ namespace Kurt\Repoist\Commands;
 use Illuminate\Console\DetectsApplicationNamespace;
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Composer;
 
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
 class RepositoryMakeCommand extends Command
 {
-    use AppNamespaceDetectorTrait;
-
+    use DetectsApplicationNamespace;
+ 
     /**
      * The console command name.
      *
